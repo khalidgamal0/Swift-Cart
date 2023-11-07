0f8f94d0a989fc78bbb118c7d2c750082f47d186
khalidgamal0 <khalidgamal7773@gmail.com>
2023-11-07 02:00:16 +0200

    edit some problem

--- lib/core/utilis/theme.dart
+++ lib/core/utilis/theme.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 ThemeData lightTheme=ThemeData(
   bottomNavigationBarTheme:  const BottomNavigationBarThemeData(
     type: BottomNavigationBarType.fixed,
-    selectedItemColor:  Color(0xff2A132E),
+    selectedItemColor: Colors.red,
     unselectedItemColor:  Color(0xff2A132E),
     backgroundColor:  Color(0xffFCF4F4),
     elevation: 20.0,
